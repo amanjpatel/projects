bc74dfe2663265d8032bf83eeb1d0e1d0a796443
amanjpatel <60860107+amanjpatel@users.noreply.github.com>
2020-02-15 15:32:09 -0800

    Add files via upload

---
+++ matching.py
@@ -0,0 +1,21 @@
+import numpy.random
+import numpy as np
+array = numpy.random.randint(0,2,(100,10))
+test = numpy.random.randint(0,2,(1,10))
+match_array = []
+print(test)
+z = 0
+num_matches = 0
+allowed_error = int(input("What % allowed error do you want"))
+for y in range(0,100):
+    for x in range(0,10):
+        num = int(array[y,x])
+        num2 = int(test[0,x])
+        if num == num2:
+            z += 1
+        if z >= 10-(allowed_error/10):
+            num_matches += 1
+            print(array[y])
+            match_array.append(array[y])
+    z = 0
+print(num_matches)
---
+++ neural_net.py
@@ -0,0 +1,42 @@
+#must be updating spike rate at all times
+#must keep track of neurotransmitter in synapse
+#must keep track of how much neurotransmitter is left in presynaptic neuron
+#must know how many ion channels on synapse
+#must be able to control either neurotransmitter or number of ion channels
+#must have a free neurotransmitter decay rate
+#must have a specified spike length
+
+# center-surround
+# orientation
+# orientation, motion, direction, position
+# orientation, motion, direction, length, position
+#
+# initial dimensions: (pixel x-value, pixel y-value, shading, time)
+# second-step dimensions: (edge x-coordinate, edge y-coordinate, edge z-coordinate, edge motion vector x-component,
+# edge motion vector y-component, edge motion vector z-component, edge length, edge width, edge depth)
+# third-intermediate dimensions: (object name, coordinated movement)
+# final dimensions: (gesture word)
+# final final dimensions: (sentences)
+
+import numpy.random
+import numpy as np
+#array = numpy.random.uniform(0,1,(10,10))
+array = np.array([[.3, .5, .8, .5, .3], [.5, .8, .5, .8, .5], [.8, .5, .3, .5, .8]])
+print(array)
+# fix lack of function on rightmost and bottommost values
+for k in range(0,100):
+    for x in range(0,4):
+        for y in range(0,2):
+            averageSurround = (array[y + 1, x] + array[y - 1, x] + array[y, x + 1] + array[y, x - 1] + array[y + 1, x + 1] +
+                           array[y - 1, x - 1] + array[y - 1, x + 1] + array[y + 1, x - 1]) / 8
+            if array[y,x] > averageSurround:
+                array[y,x] = (array[y,x])**0.5
+            else:
+                array[y,x] = (array[y,x])**2
+print(array)
+# orientation states: 8 possible orientations for a 4-pixel portion of larger array
+edgeOrient = 0
+# edge length
+# edge motion
+# shape
+# shape motion
