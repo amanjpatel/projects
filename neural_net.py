ddb5c9a98d8e55d8b4c004fa12d790fe8f0bb264
amanjpatel <amanpatel1777@gmail.com>
2020-02-19 20:19:42 -0800

    image importation fixed now the image exporting needs to be worked on

--- neural_net.py
+++ neural_net.py
@@ -23,25 +23,28 @@ import numpy as np
 from PIL import Image
 
 im = Image.open('20px.png')
-array = np.array(im)
-for x in range(0,20):
-    for y in range(0,20):
-        rgb = array[y,x]
+im_arr = np.array(im)
+print(im_arr.shape)
+
+y_dim = im_arr.shape[0]
+x_dim = im_arr.shape[1]
+
+array = np.zeros((y_dim,x_dim))
+
+for x in range(0, x_dim):
+    for y in range(0, y_dim):
+        rgb = im_arr[y,x]
         bw = 0
         for z in range(0,3):
-            rgb_z = int(rgb[z])
-            bw += rgb_z
-        bwnum = (bw/(3*255))
-        array[y,x] = bwnum
+            bw += int(rgb[z])
+        avg = (bw/(3*255))
+        array[y,x] = avg
 #array = numpy.random.uniform(0,1,(10,10))
 #array = np.array([[.3, .5, .8, .5, .3], [.5, .8, .5, .8, .5], [.8, .5, .3, .5, .8]])
 #array = np.array([[.01, .02, .01, .03, .01], [0.02, 0.03, 0.01, 0.02, 0.01], [.7, .7, .4, .7, .2]])
+a = 0
 
-print(array)
-'''
-x_dim = 20
-y_dim = 20
-for k in range(0,100):
+for k in range(0,5):
     for x in range(0,x_dim):
         for y in range(0,y_dim):
             if x == (x_dim-1) and y != y_dim-1:
@@ -54,14 +57,15 @@ for k in range(0,100):
                 averageSurround = (array[y + 1, x] + array[y - 1, x] + array[y, x + 1] + array[y, x - 1] + array[y + 1, x + 1] +
                            array[y - 1, x - 1] + array[y - 1, x + 1] + array[y + 1, x - 1]) / 8
             if array[y,x] > averageSurround:
-                array[y,x] = (array[y,x])**0.5
+                array[y,x] = (array[y,x])**0.01
             else:
-                array[y,x] = (array[y,x])**2
+                array[y,x] = (array[y,x])**100
 print(array)
+fin = Image.fromarray(array)
+Image.Image.show(fin)
 
 # orientation states
 # edge length
 # edge motion
 # shape
 # shape motion
-'''
