c5eb31e06807ef9e9ef260f87fd900292935f2c6
amanjpatel <amanpatel1777@gmail.com>
2020-02-21 11:56:40 -0800

    Basic neuron structure should work, it would be good to add a variable decay func and a variable learning func, and locality, and overfiring, and ion channels. Next step is to create timesteps, give dynamic inputs to neurons, create architectures with neurons, and receive usable output.

--- neurons2.py
+++ neurons2.py
@@ -1,16 +1,49 @@
 neur_list = []
-threshold = input("What should the threshold be?")
+neur_attr_list = []
+threshold = float(input("What should the threshold be?"))
+
 class Neuron:
+
     numNeur = 0
+
     def __init__(self, index, weights, syns):
         self.index = index
         self.weights = weights
         self.syns = syns
         Neuron.numNeur += 1
-        neur_list.append([self.index, self.weights, self.syns])
+        neur_attr_list.append([self.index, self.weights, self.syns])
 
     def activate(self, threshold):
+        sum = 0
+        # decay and addition, decay func is square root
+        for x in range(0, self.index) and range(self.index + 1, len(self.syns)):
+            self.syns[x] = (self.syns[x])**0.5
+            self.syns[x] = (self.syns[x]) + ((self.weights[x]) * (fires[x]))
+            sum += self.syns[x]
+        # firing
+        if sum >= threshold:
+            fires[self.index] = 1
+        else:
+            fires[self.index] = 0
+        # learning, learning func is square/square root
+        for x in range(0, self.index) and range(self.index + 1, len(self.syns)):
+            if fires[x] and fires[self.index] == 1:
+                if self.weights[x] >= 0:
+                    self.weights[x] = (self.weights[x]) ** 0.5
+                else:
+                    self.weights[x] = -1 * ((abs(self.weights[x])) ** 0.5)
+            elif fires[x] != fires[self.index]:
+                if self.weights[x] >= 0:
+                    self.weights[x] = (self.weights[x]) ** 2
+                else:
+                    self.weights[x] = -1 * ((self.weights[x]) ** 2)
 
 
-neuron1 = Neuron(Neuron.numNeur, [.5,.5,.5],[0,0,0])
-neuron2 = Neuron(Neuron.numNeur, [.5,.5,.5],[0,0,0])
+fires = [0] * Neuron.numNeur
+
+userNum = int(input("How many neurons do you want?"))
+initialWeights = float(input("What initial weight?"))
+
+for x in range(0, userNum):
+    neur_list.append(Neuron(Neuron.numNeur, [initialWeights] * Neuron.numNeur, [0] * Neuron.numNeur))
+print(neur_list)
