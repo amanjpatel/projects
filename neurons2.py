a3320577e0e55da03542d1ea54c23e13daa9b7e1
amanjpatel <amanpatel1777@gmail.com>
2020-02-21 16:16:07 -0800

    something wacky with self.index, needs fixing, but besides that the cycles have been added

--- neurons2.py
+++ neurons2.py
@@ -1,5 +1,6 @@
 neur_list = []
 neur_attr_list = []
+attr = []
 threshold = float(input("What should the threshold be?"))
 
 class Neuron:
@@ -13,13 +14,16 @@ class Neuron:
         Neuron.numNeur += 1
         neur_attr_list.append([self.index, self.weights, self.syns])
 
-    def activate(self, threshold):
+    @classmethod
+    def activate(self):
         sum = 0
         # decay and addition, decay func is square root
         for x in range(0, self.index) and range(self.index + 1, len(self.syns)):
             self.syns[x] = (self.syns[x])**0.5
             self.syns[x] = (self.syns[x]) + ((self.weights[x]) * (fires[x]))
+            print(self.syns[x])
             sum += self.syns[x]
+            print(sum)
         # firing
         if sum >= threshold:
             fires[self.index] = 1
@@ -38,12 +42,24 @@ class Neuron:
                 else:
                     self.weights[x] = -1 * ((self.weights[x]) ** 2)
 
-
-fires = [0] * Neuron.numNeur
-
+# set neuron number and initial weights
 userNum = int(input("How many neurons do you want?"))
 initialWeights = float(input("What initial weight?"))
+cycles = int(input("How many cycles?"))
 
+# create neurons
 for x in range(0, userNum):
     neur_list.append(Neuron(Neuron.numNeur, [initialWeights] * Neuron.numNeur, [0] * Neuron.numNeur))
-print(neur_list)
+
+# set initial firing pattern
+#fires = [0] * Neuron.numNeur
+fires = [0, 1, 0, 0, 1]
+
+# activate neurons
+for x in range(0, cycles):
+    Neuron.activate()
+
+# print resulting neuron states
+for x in range(0, len(neur_list)):
+    attr.append([neur_list[x].index, neur_list[x].weights, neur_list[x].syns])
+print(attr)
