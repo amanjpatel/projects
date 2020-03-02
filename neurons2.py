8bdec364a5a810bc514c8786b3442b58a5439f5e
amanjpatel <amanpatel1777@gmail.com>
2020-03-01 16:41:33 -0800

    added a strange version of locality

--- neurons2.py
+++ neurons2.py
@@ -1,8 +1,9 @@
-# add locality/topology
+# add better version of locality
 # add layers
 # add nonlinear weighting, so that it is more difficult for unconnected neurons to become connected
 # add a more accurate decay function
 # synchronize timescales
+# create types of neurons by tweaking parameters: local-only neurons, wide neurons, etc.
 # (next major phase): organize neurons into homeostatic microcolumns
 # outstanding questions:
 #   1. How should the initial weights be defined, in order to preserve homeostasis?
@@ -20,18 +21,22 @@ attr = []
 # set firing threshold
 threshold = float(input("What should the threshold be?"))
 
+# set distance for locality
+locality_thresh = float(input("What is the maximum distance to be considered local?"))
+
 class Neuron:
 
     # counter for number of neurons instantiated
     numNeur = 0
-    # constructor for neuron, with an index, a weight list, and a synapse list
+    # constructor for neuron, with an index, a weight list, a synapse list, and a 3D location tuple
     # Weight list contains all presynaptic weights, synapse list contains amount of neurotransmitter in each synapse
-    def __init__(self, index, weights, syns):
+    def __init__(self, index, weights, syns, pos):
         self.index = index
         self.weights = weights
         self.syns = syns
+        self.pos = pos
         Neuron.numNeur += 1
-        neur_attr_list.append([self.index, self.weights, self.syns])
+        neur_attr_list.append([self.index, self.weights, self.syns, self.pos])
     # activation method
     @staticmethod
     def activate(self):
@@ -75,27 +80,34 @@ class Neuron:
     @staticmethod
     def learn(self):
         for x in range(0, self.index) and range(self.index + 1, len(self.syns)):
-            # if a presynaptic neuron fires and the self fires, increase the weight of the presynaptic neuron
-            if fires[x] and fires[self.index] == 1:
-                if self.weights[x] >= 0:
-                    self.weights[x] = self.weights[x] ** 0.5
-                else:
-                    self.weights[x] = -1 * ((abs(self.weights[x])) ** 0.5)
-            # if not, then reduce the weight
-            elif fires[x] != fires[self.index]:
-                if self.weights[x] >= 0:
-                    self.weights[x] = self.weights[x] ** 2
-                else:
-                    self.weights[x] = -1 * (self.weights[x] ** 2)
+            # if the neurons are close together, then execute the learning function
+            # calculate distance between presynaptic and postsynaptic neuron
+            dist = (((self.pos[0] - neur_list[x].pos[0]) ** 2) + ((self.pos[1] - neur_list[x].pos[1]) ** 2) + ((self.pos[0] - neur_list[x].pos[0]) ** 2)) ** 0.5
+            if dist <= locality_thresh:
+                # if a presynaptic neuron fires and the self fires, increase the weight of the presynaptic neuron
+                if fires[x] and fires[self.index] == 1:
+                    if self.weights[x] >= 0:
+                        self.weights[x] = self.weights[x] ** 0.5
+                    else:
+                        self.weights[x] = -1 * ((abs(self.weights[x])) ** 0.5)
+                # if not, then reduce the weight
+                elif fires[x] != fires[self.index]:
+                    if self.weights[x] >= 0:
+                        self.weights[x] = self.weights[x] ** 2
+                    else:
+                        self.weights[x] = -1 * (self.weights[x] ** 2)
+            else:
+                # if they aren't local, weights should be zero
+                self.weights[x] = 0
 
 
-# set neuron number and initial weights
+# set neuron number and number of timesteps
 userNum = int(input("How many neurons do you want?"))
 cycles = int(input("How many cycles?"))
 
 # ~create neurons~
-# set initial weights
 for x in range(0, userNum):
+    #set initial weights
     #print("What initial weight list? Must have " + str(userNum) + " items")
     initialWeights = []
     #initialWeights = [0.5] * userNum
@@ -103,8 +115,13 @@ for x in range(0, userNum):
     for z in range(0, userNum):
         #initialWeights.append(float(input("Weight for element " + str(z) + "? (from -1 to 1)")))
         initialWeights.append(nprand.uniform(-1, 1))
+
+    # create a random position tuple with three random numbers, for a space with bounds of -1 and 1 for each dimension
+    pos_tuple = (float(nprand.uniform(-1, 1)), float(nprand.uniform(-1, 1)), float(nprand.uniform(-1, 1)))
+
     # instantiate neurons with arguments numNeur, list initialWeights, and an empty neurotransmitter list for each synapse
-    neur_list.append(Neuron(Neuron.numNeur, initialWeights, [0] * userNum))
+    # and three spatial coordinates
+    neur_list.append(Neuron(Neuron.numNeur, initialWeights, [0] * userNum, pos_tuple))
 
 # set initial firing pattern (must be same length as numNeur)
 #fires = [0] * Neuron.numNeur
@@ -125,6 +142,6 @@ for y in range(0, cycles):
 
 # print resulting neuron states
 for x in range(0, len(neur_list)):
-    attr.append([neur_list[x].index, neur_list[x].weights, neur_list[x].syns])
+    attr.append([neur_list[x].index, neur_list[x].weights, neur_list[x].syns, neur_list[x].pos])
 #print(attr)
 print(fires)
